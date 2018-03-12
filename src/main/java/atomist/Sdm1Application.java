8127a3975bee037d99e12be881adcb68284f1387
Rod Johnson <johnsonroda@gmail.com>
2018-03-13 08:05:19 +1100

    Initial commit from Atomist

---
+++ src/main/java/atomist/Sdm1Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdm1Application.class, args);
+	}
+}
